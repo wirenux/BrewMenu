929d424e0f866339b1d3f16e8ac3c50a4c0f925f
Wirenux <louis.lesniak2@gmail.com>
2026-06-07 15:59:18 +0200

    refactor: restructure main function and improve background drawing

--- src/main.c
+++ src/main.c
@@ -3,62 +3,58 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main(void) {
-    int capacity = 32;
-    int total_packages = 0;
+#define MENU_WIDTH 45
+#define MENU_HEIGHT 10
 
-    char **packages = malloc(capacity * sizeof(char *));
+void draw_background(int max_y, int max_x) {
+    attron(COLOR_PAIR(3) | A_BOLD);
+    for (int x = 0; x < max_x; x++) {
+        mvaddch(0, x, ' ');
+    }
+
+    char *title = "BrewMenu";
+    mvprintw(0, (max_x - (int)strlen(title)) / 2, "%s", title);
+    attroff(COLOR_PAIR(3) | A_BOLD);
+
+    attron(COLOR_PAIR(3));
+    mvprintw(max_y - 1, 0, " Use Arrow Keys to navigate, [ENTER] to select, 'q' to quit");
+
+    for (int x = 60; x < max_x; x++) {
+        mvaddch(max_y - 1, x, ' ');
+    }
     
-    if (packages == NULL) {
-        puts("\e[1;31mError:\e[0m Failed to allocate inital memory");
-        return 1;
-    }
+    attroff(COLOR_PAIR(3));
+}
 
-    FILE *fp = popen("brew list -1", "r");
-    if (fp == NULL) {
-        puts("\e[1;31mError:\e[0m Failed to run the brew command");
-        free(packages);
-        return 1;
-    }
-
-    char line[64];
-    while (fgets(line, sizeof(line), fp) != NULL) {
-        line[strcspn(line, "\n")] = '\0';
-
-        if (total_packages >= capacity) {
-            capacity *= 2;
-            char **temp = realloc(packages, capacity * sizeof(char *));
-            if (temp == NULL) {
-                pclose(fp);
-                for (int i = 0; i < total_packages; i++) {
-                    free(packages[i]);
-                }
-                free(packages);
-                perror("\e[1;31mError:\e[0m Failed to reallocate memory");
-                return 1;
-            }
-            packages = temp;
-        }
-
-        packages[total_packages] = strdup(line);
-        total_packages++;
-    }
-    pclose(fp);
-
-    if (total_packages == 0) {
-        puts("\e[1;31mError:\e[0m No brew packages found");
-        free(packages);
-        return 1;
-    }
-
+int main(void) {
     initscr();
     cbreak();
     noecho();
     curs_set(0);
     keypad(stdscr, TRUE);
 
+    if (!has_colors()) {
+        endwin();
+        puts("\e[1;31mError:\e[0m Your terminal doesn't support colors!\n");
+        return 1;
+    }
+    start_color();
+
+    init_pair(1, COLOR_WHITE, COLOR_BLUE); // bg
+    init_pair(2, COLOR_BLACK, COLOR_WHITE); // body
+    init_pair(3, COLOR_WHITE, COLOR_BLACK); // shadow
+
+    wbkgd(stdscr, COLOR_PAIR(1));
+
+    char *main_option[] = {
+        "Install packages",
+        "Search packages",
+        "List installed packages",
+        "Uninstall packages"
+    };
+
+    int total_option = 4;
     int selected_index = 0;
-    int scroll_offset = 0;
     int ch;
 
     while (1) {
@@ -67,55 +63,53 @@ int main(void) {
         int max_x, max_y;
         getmaxyx(stdscr, max_y, max_x);
 
-        int visible_rows = max_y - 3;
+        draw_background(max_y, max_x);
+        
+        int start_y = (max_y - MENU_HEIGHT) / 2;
+        int start_x = (max_x - MENU_WIDTH) / 2;
 
-        mvprintw(0, 2, "=== Brew Menu (Total: %d) ===", total_packages);
+        attron(COLOR_PAIR(3));
+        for (int i = 0; i < MENU_HEIGHT; i++) {
+            mvprintw(start_y + i + 1, start_x + MENU_WIDTH, "  "); // right shadow
+        }
+        for (int i = 0; i < MENU_WIDTH; i++) {
+            mvaddch(start_y + MENU_HEIGHT, start_x + i + 1, ' '); // right shadow
+        }
+        attroff(COLOR_PAIR(3));
 
-        for (int i = 0; i < visible_rows; i++) {
-            int pkg_index = scroll_offset + i;
+        WINDOW *menu_win = newwin(MENU_HEIGHT, MENU_WIDTH, start_y, start_x);
+        wbkgd(menu_win, COLOR_PAIR(2));
+        box(menu_win, 0, 0);
 
-            if (pkg_index >= total_packages) {
-                break;
+        mvwprintw(menu_win, 0, (MENU_WIDTH - 13) / 2, "[ Main Menu ]");
+
+        for (int i = 0; i < total_option; i++) {
+            if (i == selected_index) {
+                wattron(menu_win, COLOR_PAIR(3));
             }
 
-            if (pkg_index == selected_index) {
-                attron(A_REVERSE); // highlight turn on
-            }
+            mvwprintw(menu_win, i + 3, 4, "  %s  ", main_option[i]);
 
-            mvprintw(i + 2, 2, "%d - [ %s ]", pkg_index + 1 ,packages[pkg_index]); // index - [ pkg_name ]
-
-            if (pkg_index == selected_index) {
-                attroff(A_REVERSE); // highlight turn off
+            if (i == selected_index) {
+                wattroff(menu_win, COLOR_PAIR(3));
             }
         }
 
         refresh();
+        wrefresh(menu_win);
 
         ch = getch();
+        delwin(menu_win);
 
         if (ch == 'q') {
             break;
         } else if (ch == KEY_UP && selected_index > 0) {
             selected_index--;
-
-            if (selected_index < scroll_offset) {
-                scroll_offset = selected_index;
-            }
-        } else if (ch == KEY_DOWN && selected_index < total_packages - 1) {
+        } else if (ch == KEY_DOWN && selected_index < total_option - 1) {
             selected_index++;
-
-            if (selected_index >= scroll_offset + visible_rows) {
-                scroll_offset = selected_index - visible_rows + 1;
-            }
         }
     }
 
     endwin();
-
-    for (int i = 0; i < total_packages; i++) {
-        free(packages[i]);
-    }
-    free(packages);
-
     return 0;
 }
