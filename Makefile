a0b21c7bc37d700b9fd831e17405fc8e97706cf1
Wirenux <louis.lesniak2@gmail.com>
2026-06-07 14:36:30 +0200

    add: dummy data and boilerplate

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ run:
 
 build: clean
 	@mkdir -p build
-	@gcc src/main.c -o build/brewmenu
+	@gcc src/main.c -lncurses -o build/brewmenu
 
 clean:
 	@rm -rf build/
--- src/main.c
+++ src/main.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
+#include <ncurses.h>
 
 int main(void) {
-    puts("Hello !");
+    initscr();
+    cbreak();
+    noecho();
+    curs_set(0);
+    keypad(stdscr, TRUE);
+
+    // dummy data
+    char *packages[] = {"fastfetch", "nvim", "wget", "btop", "quickstart"};
+    int total_packages = 5;
+    int selected_index = 0;
+
+    int ch;
+
+    while (1) {
+        clear();
+
+        mvprintw(0, 2, "=== Brew Menu ===");
+
+        for (int i = 0; i < total_packages; i++) {
+            if (i == selected_index) {
+                attron(A_REVERSE); // highlight turn on
+            }
+
+            mvprintw(i + 2, 2, "[ %s ]", packages[i]);
+
+            if (i == selected_index) {
+                attroff(A_REVERSE); // highlight turn off
+            }
+        }
+
+        refresh();
+
+        ch = getch();
+
+        if (ch == 'q') {
+            break;
+        } else if (ch == KEY_UP && selected_index > 0) {
+            selected_index--;
+        } else if (ch == KEY_DOWN && selected_index < total_packages - 1) {
+            selected_index++;
+        }
+    }
+
+    endwin();
     return 0;
 }
