5575b64aac3c16d29a200a5a68b82a7f56b0b15e
Wirenux <louis.lesniak2@gmail.com>
2026-06-07 14:54:39 +0200

    add: able to get more than 256 packages & automatic memory management

--- src/main.c
+++ src/main.c
@@ -1,16 +1,62 @@
 #include <stdio.h>
 #include <ncurses.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(void) {
+    int capacity = 32;
+    int total_packages = 0;
+
+    char **packages = malloc(capacity * sizeof(char *));
+    
+    if (packages == NULL) {
+        puts("\e[1;31mError:\e[0m Failed to allocate inital memory");
+        return 1;
+    }
+
+    FILE *fp = popen("brew list -1", "r");
+    if (fp == NULL) {
+        puts("\e[1;31mError:\e[0m Failed to run the brew command");
+        free(packages);
+        return 1;
+    }
+
+    char line[64];
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        line[strcspn(line, "\n")] = '\0';
+
+        if (total_packages >= capacity) {
+            capacity *= 2;
+            char **temp = realloc(packages, capacity * sizeof(char *));
+            if (temp == NULL) {
+                pclose(fp);
+                for (int i = 0; i < total_packages; i++) {
+                    free(packages[i]);
+                }
+                free(packages);
+                perror("\e[1;31mError:\e[0m Failed to reallocate memory");
+                return 1;
+            }
+            packages = temp;
+        }
+
+        packages[total_packages] = strdup(line);
+        total_packages++;
+    }
+    pclose(fp);
+
+    if (total_packages == 0) {
+        puts("\e[1;31mError:\e[0m No brew packages found");
+        free(packages);
+        return 1;
+    }
+
     initscr();
     cbreak();
     noecho();
     curs_set(0);
     keypad(stdscr, TRUE);
 
-    // dummy data
-    char *packages[] = {"fastfetch", "nvim", "wget", "btop", "quickstart"};
-    int total_packages = 5;
     int selected_index = 0;
 
     int ch;
@@ -46,5 +92,11 @@ int main(void) {
     }
 
     endwin();
+
+    for (int i = 0; i < total_packages; i++) {
+        free(packages[i]);
+    }
+    free(packages);
+
     return 0;
 }
